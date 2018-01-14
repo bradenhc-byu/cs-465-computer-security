b99398080b6f0109954f10b182d05f157d269e63
Braden Hitchcock <hitchcock.braden@gmail.com>
2018-01-13 21:35:59 -0700

    Implemented pseudocode and ffmultiply

--- homework/h01/FiniteFieldArithmetic/src/main.cpp
+++ homework/h01/FiniteFieldArithmetic/src/main.cpp
@@ -1,6 +1,90 @@
+/**
+ * Homework 2 - Finite Field Arithmetic
+ * @author Braden Hitchcock
+ * @date 1/16/2018
+ *
+ * This source file contains commented pseudocode of the AES Mix Columns implementation as well as working C++ code
+ * for a finite field multiplication function.
+ *
+ * Pseudocode for AES Mix Columns Function -----------------------------------------------------------------------------
+ *
+ * INPUT:   The original state of the message as a 4 x 4 array of bytes
+ * OUTPUT:  The transformed state of the message as a 4 x 4 array of bytes
+ *
+ *  MixColumns(state){
+ *      a =: an array of the coefficients of the fixed polynomial  3x^3 + x^2 + x + 2, thus [3,1,1,2])
+ *      result_state =: a copy of the input byte matrix
+ *      c =: the column counter, initialized to 0
+ *      for c < 4{
+ *          result_state[0][c] = ffmult(a[3], state[0][c]) xor ffmult(a[0], state[1][c]) xor state[2][c] xor state[3][c]
+ *          result_state[1][c] = state[0][c] xor ffmult(a[3], state[1][c]) xor ffmult(a[0], state[2][c]) xor state[3][c]
+ *          result_state[2][c] = state[0][c] xor state[1][c] xor ffmult(a[3], state[2][c]) xor ffmult(a[0], state[3][c])
+ *          result_state[3][c] = ffmult(a[0], state[0][c]) xor state[1][c] xor state[2][c] xor ffmult(a[x], state[3]c[])
+ *          c = c + 1
+ *      }
+ *      return result_state
+ *  }
+ *
+ * Pseudocode for Finite Field Multiplication function used in AES Mixed Columns Function
+ *
+ * INPUT: two bytes a and b
+ * OUTPUT: one byte representing the multiplication of a and b modulo an irreducible polynomial x^8 + x^4 + x^3 + x + 1
+ *
+ *  ffmultiply(a, b){
+ *
+ *      result =: 0
+ *      count =: 0
+ *      for count < 8 {
+ *          if the low bit in b is set {
+ *              add 'a' to the result (xor)
+ *          }
+ *          Check if the high bit is set in a
+ *          Shift a to the left one bit
+ *          If the high bit was set {
+ *              xor a with 0x1B (the lower 8 bits of the irreducible polynomial) to prevent overflow,
+ *                  since the result is only 8 bits
+ *          }
+ *          Shift b to the right 1
+ *          count = count + 1
+ *      }
+ *      return result
+ *
+ *  }
+ *
+ * ---------------------------------------------------------------------------------------------------------------------
+ */
+
+// Include necessary headers
 #include <iostream>
 
+// Create byte typedef for aesthetics
+typedef unsigned char Byte;
+
+/** Finite Field Multiplication function */
+Byte ffmultiply(Byte a, Byte b){
+    // Initialize the result
+    Byte result = 0x00;
+    // Declare variable for keeping track of whether the high bit is set or not
+    Byte highBitSet;
+    // Perform the multiplication
+    while(b){
+        if((b & 1) != 0){
+            result ^= a;
+        }
+        highBitSet = (Byte)(a & 0x80);
+        a <<= 1;
+        if(highBitSet != 0){
+            a ^= 0x1B;
+        }
+        b >>= 1;
+    }
+    return result;
+}
+
+// Main execution
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << (int)ffmultiply(0x57, 0x83) << std::endl;
     return 0;
 }
+
+// RESULT: 0xC1 (or 193)
